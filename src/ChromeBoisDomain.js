6fd1ed2e9a66cd273da8a8a8dfdb6e1e50b262f4
Patrick Kim <pkim050@gmail.com>
2019-07-09 10:14:26 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -40,6 +40,8 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas
         onMouseMove={this.handleMouseMove}
+        onClick={this.handleClick}
+        onKeyDown={this.handleKey}
         width='900'
         height='600'
         tabIndex="0">
